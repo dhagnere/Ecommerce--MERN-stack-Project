392e6a0bbcc9ea407bf9daac1d2cca1f7c554221
dhagnere <da.hagnere@gmail.com>
2022-12-01 14:34:41 +0100

    Style Footer Component
    Use of grid to center on bottom footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,5 +1,17 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 const FooterComponent = () => {
-  return <p>This is a footer</p>
+  return (
+    <footer>
+      <Container fluid>
+        <Row className='bg-dark text-white text-center py-5 mt-5'>
+          <Col>Copyright &copy; Dhagnere</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
 
 export default FooterComponent
