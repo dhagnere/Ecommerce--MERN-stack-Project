5dbc1115b5a2094880c5a819a4901b9864232adc
dhagnere <da.hagnere@gmail.com>
2022-12-05 11:38:58 +0100

    Chat Popup – Blue Circle With Icons
    Creating buttons for chat.
    Creating a check state => enable or disable the visibilty of icon

--- src/components/user/UserChatComponent.js
+++ src/components/user/UserChatComponent.js
@@ -1,5 +1,15 @@
+import "../../chats.css"
+
 const UserChatComponent = () => {
-  return <p>this is user Chat component</p>
+  return (
+    <>
+      <input type='checkbox' id='check' />
+      <label className='chat-btn' htmlFor='check'>
+        <i className='bi bi-chat-dots comment'></i>
+        <i className='bi bi-x-circle close'></i>
+      </label>
+    </>
+  )
 }
 
 export default UserChatComponent
