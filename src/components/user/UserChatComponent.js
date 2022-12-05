02c3b6a10f56e08e70ca877182faf08f98bac1cd
dhagnere <da.hagnere@gmail.com>
2022-12-05 15:23:59 +0100

    Chat Popup – Create Header

--- src/components/user/UserChatComponent.js
+++ src/components/user/UserChatComponent.js
@@ -8,6 +8,11 @@ const UserChatComponent = () => {
         <i className='bi bi-chat-dots comment'></i>
         <i className='bi bi-x-circle close'></i>
       </label>
+      <div className="chat-wrapper">
+        <div className="chat-header">
+          <h6>Nous somme à votre écoute.</h6>
+        </div>
+      </div>
     </>
   )
 }
