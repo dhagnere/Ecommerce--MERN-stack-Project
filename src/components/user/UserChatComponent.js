b3eeeedcdc6ac04da3c8375529b71500c1fd87ff
dhagnere <da.hagnere@gmail.com>
2022-12-06 09:22:22 +0100

    Chat Popup - Exemple History
    Simulation of messages (hard coded)
    Chat notification

--- src/components/user/UserChatComponent.js
+++ src/components/user/UserChatComponent.js
@@ -1,4 +1,5 @@
-import "../../chats.css"
+import '../../chats.css'
+import Button from 'react-bootstrap/Button'
 
 const UserChatComponent = () => {
   return (
@@ -6,11 +7,32 @@ const UserChatComponent = () => {
       <input type='checkbox' id='check' />
       <label className='chat-btn' htmlFor='check'>
         <i className='bi bi-chat-dots comment'></i>
+        <span className='position-absolute top-0 start-10 translate-middle p-2 bg-danger boder border-light rounded-circle'></span>
         <i className='bi bi-x-circle close'></i>
       </label>
-      <div className="chat-wrapper">
-        <div className="chat-header">
+      <div className='chat-wrapper'>
+        <div className='chat-header'>
           <h6>Nous somme à votre écoute.</h6>
+        </div>
+        <div className='chat-form'>
+          <div className='chat-msg'>
+            {Array.from({ length: 20 }).map((_, id) => (
+              <div>
+                <p>
+                  <b>Vous : </b> Bonjour le monde ceci est un test
+                </p>
+                <p className='bg-primary p-3 ms-4 text-light rounded-pill'>
+                  <b>Support: Bienvenue au test ^^</b>
+                </p>
+              </div>
+            ))}
+          </div>
+          <textarea
+            id='clientChatMsg'
+            className='form-control'
+            placeholder='Entrez votre texte...'
+          ></textarea>
+          <Button className='btn btn-success btn-block'>Envoyer</Button>
         </div>
       </div>
     </>
