76e5f5c39da89f3d99384bb2dc9b56be4fa289ab
dhagnere <da.hagnere@gmail.com>
2022-12-07 16:08:28 +0100

    Update CategoryFilterComponent.js
    Improve component for category

--- src/components/CategoryFilterComponent.js
+++ src/components/CategoryFilterComponent.js
@@ -2,17 +2,21 @@ import { Form } from 'react-bootstrap'
 
 const CategoryFilterComponent = () => {
   return (
-    <Form>
-      {['checkbox ' , 'radio'].map((type) => (
-        <div key={type} className='mb-3'>
-          <Form.Check type={type} id={`check-api-${type}`}>
-            <Form.Check.Input type={type} isValid/>
-            <Form.Check.Label>{`Custom.api ${type}`}</Form.Check.Label>
-            <Form.Control.Feedback type='valid'>Bravo</Form.Control.Feedback>
-          </Form.Check>
-        </div>
-      ))}
-    </Form>
+    <>
+      <span className='fw-bold'>Catégories</span>
+      <Form>
+        {Array.from({ length: 5 }).map((_, idx) => (
+          <div key={idx}>
+            <Form.Check type='checkbox' id={`check-api2-${idx}`}>
+              <Form.Check.Input type='checkbox' isValid />
+              <Form.Check.Label style={{ cursor: 'pointer' }}>
+                Catégorie -{idx}
+              </Form.Check.Label>
+            </Form.Check>
+          </div>
+        ))}
+      </Form>
+    </>
   )
 }
 
