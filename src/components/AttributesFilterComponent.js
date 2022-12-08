d2d387ebe45c3c3b608b4041405b9197ebc58398
dhagnere <da.hagnere@gmail.com>
2022-12-08 09:34:50 +0100

    Improve Attributes Section for Filtering Options

--- src/components/AttributesFilterComponent.js
+++ src/components/AttributesFilterComponent.js
@@ -3,13 +3,25 @@ import { Form } from 'react-bootstrap'
 const AttributesFilterComponent = () => {
   return (
     <>
-      <Form.Label>Color</Form.Label>
-      <Form.Check
-        type="checkbox"
-        id="default-checkbox"
-        label="vert"
-      />
-   </>
+      {[
+        { color: ['red', 'blue', 'green'] },
+        { ram: ['1 TB', '2 TB', '4 TB'] },
+      ].map((item, idx) => (
+        <div key={idx} className='mb-3'>
+          <Form.Label>
+            <b>{Object.keys(item)}</b>
+          </Form.Label>
+          {item[Object.keys(item)].map((i, idx) => (
+            <Form.Check
+              key='idx'
+              type='checkbox'
+              id='default-checkbox'
+              label={i}
+            />
+          ))}
+        </div>
+      ))}
+    </>
   )
 }
 
--- src/pages/ProductListPage.js
+++ src/pages/ProductListPage.js
@@ -28,8 +28,8 @@ const ProductListPage = () => {
             <ListGroup.Item>{<CategoryFilterComponent />}</ListGroup.Item>
             <ListGroup.Item>{<AttributesFilterComponent />}</ListGroup.Item>
             <ListGroup.Item>
-              <Button variant='primary'>Primary</Button>
-              <Button variant='danger'>Danger</Button>
+              <Button variant='primary'>Filter</Button>
+              <Button variant='danger'>Reset filters</Button>
             </ListGroup.Item>
           </ListGroup>
         </Col>
