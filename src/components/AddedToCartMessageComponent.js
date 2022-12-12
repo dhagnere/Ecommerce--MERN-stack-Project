1e8042d3c4742eb6e8fcee3de6932119e8f31cd9
dhagnere <da.hagnere@gmail.com>
2022-12-12 13:47:54 +0100

    Update Product Description Page
    Working around styles

--- src/components/AddedToCartMessageComponent.js
+++ src/components/AddedToCartMessageComponent.js
@@ -1,18 +1,24 @@
 import { useState } from 'react'
-import { Alert } from 'react-bootstrap'
+import { Alert, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const AddedToCartMessageComponent = () => {
-
-  const [ show, setShow ] = useState(true)
+  const [show, setShow] = useState(true)
   return (
-    <Alert show={show} variant="danger" onClose={() => setShow(false)} dismissible>
-        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-        <p>
-          Change this and that and try again. Duis mollis, est non commodo
-          luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-          Cras mattis consectetur purus sit amet fermentum.
-        </p>
-      </Alert>
+    <Alert
+      show={show}
+      variant='success'
+      onClose={() => setShow(false)}
+      dismissible
+    >
+      <Alert.Heading>L'article à été ajouté à votre panier.</Alert.Heading>
+      <p>
+        <Button variant='success'>Retour</Button>{" "}
+        <Link to='/cartPage'>
+          <Button variant='danger'>Voir le panier</Button>
+        </Link>
+      </p>
+    </Alert>
   )
 }
 
--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -1,79 +1,109 @@
-import { Row, Col, Container, Image , ListGroup,Form, Button, Alert} from 'react-bootstrap'
+import {
+  Row,
+  Col,
+  Container,
+  Image,
+  ListGroup,
+  Form,
+  Button,
+  Alert,
+} from 'react-bootstrap'
 import AddedToCartMessageComponent from '../components/AddedToCartMessageComponent'
-import {Rating} from "react-simple-star-rating"
-
+import { Rating } from 'react-simple-star-rating'
 
 const ProductDetailsPage = () => {
   return (
     <Container>
-      <AddedToCartMessageComponent/>
-       <Row className='mt-5'>
+      <AddedToCartMessageComponent />
+      <Row className='mt-5'>
         <Col md={4}>
-          <Image fluid src="/images/games-category.png" />
-          <Image fluid src="/images/monitors-category.png" />
-          <Image fluid src="/images/tablets-category.png" />
-          <Image fluid src="/images/games-category.png" />
+          <Image fluid src='/images/games-category.png' />
+          <Image fluid src='/images/monitors-category.png' />
+          <Image fluid src='/images/tablets-category.png' />
+          <Image fluid src='/images/games-category.png' />
         </Col>
-      <Col md={8}>
-        <Row>
+        <Col md={8}>
+          <Row>
             <Col md={8}>
-              <ListGroup variant="flush">
-                <ListGroup.Item>LOREM IPSUM</ListGroup.Item>
-                <ListGroup.Item><Rating readonly size={20} initialValue={4} /></ListGroup.Item>
-                <ListGroup.Item>Blah BLah</ListGroup.Item>
-                <ListGroup.Item>Blah BLah</ListGroup.Item>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <h1>Nom du produit.</h1>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating readonly size={20} initialValue={4} />
+                  (1)
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Prix : <span className='fw-bold'>245 € </span>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  velit tortor, aliquet sit amet rhoncus nec, facilisis ac leo.
+                  Vestibulum ante ipsum primis in faucibus orci luctus et
+                  ultrices posuere cubilia curae; Mauris vel viverra ex, eu
+                  aliquet mi. Nullam ut gravida elit.{' '}
+                </ListGroup.Item>
               </ListGroup>
             </Col>
 
-          <Col md={4}><ListGroup>
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <Col md={4}>
+              <ListGroup>
+                <ListGroup.Item>Status : En stock.</ListGroup.Item>
                 <ListGroup.Item>
-                  <Form.Select size="lg" aria-label="Default select example">
-                    <option>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                  Prix : <span className='fw-bold'> 245 €</span>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Quantité :
+                  <Form.Select size='lg' aria-label='Default select example'>
+                    <option>1</option>
+                    <option value='1'>2</option>
+                    <option value='2'>3</option>
+                    <option value='3'>4</option>
                   </Form.Select>
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  <Button variant="danger">Danger</Button>
+                  <Button variant='danger'>Ajouter au panier</Button>
                 </ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-              </ListGroup></Col>
-        </Row>
-        <Row>
-          <Col className="mt-5">
+              </ListGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col className='mt-5'>
               <h5>AVIS CLIENTS</h5>
-              <ListGroup variant="flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+              <ListGroup variant='flush'>
+                {Array.from({ length: 10 }).map((item, idx) => (
+                  <ListGroup.Item key={idx}>
+                    John Doe <br />
+                    <Rating readonly size={20} initialValue={4}></Rating>
+                    <br />
+                    12/12/2022 <br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Fusce velit tortor, aliquet sit amet rhoncus nec, facilisis
+                    ac leo.
+                  </ListGroup.Item>
+                ))}
               </ListGroup>
             </Col>
           </Row>
           <hr />
-          <Alert variant="danger">Vous devez vous connecter pour laisser un avis</Alert>
+          <Alert variant='danger'>
+            Vous devez vous connecter pour laisser un avis.
+          </Alert>
           <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Addresse Mail</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
+            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
               <Form.Label>Votre avis est important :</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control as='textarea' rows={3} />
             </Form.Group>
-            <Form.Select aria-label="Default select example">
-              <option>Veuillez selectionner une option</option>
-              <option value="1">Option-1</option>
-              <option value="2">Option-2</option>
-              <option value="3">Option-3</option>
+            <Form.Select aria-label='Default select example'>
+              <option>Notation sur le produit</option>
+              <option value='5'>5 (très bon)</option>
+              <option value='4'>4 (bon)</option>
+              <option value='3'>3 (moyen)</option>
+              <option value='2'>2 (mauvais)</option>
+              <option value='1'>1 (très mauvais)</option>
+             
             </Form.Select>
-            <Button variant="primary">Primary</Button>
+            <Button className="mb-3 mt-3"variant='primary'>Envoi</Button>
           </Form>
         </Col>
       </Row>
