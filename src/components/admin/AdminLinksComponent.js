35ce3f0581f7dc64bec2e03c68bf493bb3f7ccdb
dhagnere <da.hagnere@gmail.com>
2023-01-06 11:35:09 +0100

    Prepare Admin orders page
    Create AdminLinksComponent
    implent AdminLinksComponent as menu on left part of page.

---
+++ src/components/admin/AdminLinksComponent.js
@@ -0,0 +1,29 @@
+import { Nav, Navbar } from 'react-bootstrap'
+import { Link, LinkContainer } from 'react-router-bootstrap'
+
+const AdminLinksComponent = () => {
+  return (
+    <Navbar bg='light' variant='light'>
+      <Nav className='flex-column'>
+        <LinkContainer to='/admin/orders'>
+          <Nav.Link>Commandes</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/admin/products'>
+          <Nav.Link>Produits</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/admin/users'>
+          <Nav.Link>Liste des clients</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/admin/chats'>
+          <Nav.Link>Chats</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to='/admin/analytics'>
+          <Nav.Link>Analyse</Nav.Link>
+        </LinkContainer>
+        <Nav.Link>Déconnexion</Nav.Link>
+      </Nav>
+    </Navbar>
+  )
+}
+
+export default AdminLinksComponent
--- src/pages/admin/AdminOrdersPage.js
+++ src/pages/admin/AdminOrdersPage.js
@@ -1,5 +1,50 @@
+import { Row, Col, Table } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import AdminLinksComponent from '../../components/admin/AdminLinksComponent'
+
 const AdminOrdersPage = () => {
-  return <p>This is Admin Orders Page</p>
+  return (
+    <Row className='m-5'>
+      <Col md={2}>
+        <AdminLinksComponent/>
+      </Col>
+      <Col md={10}>
+        <h1>Mes commandes</h1>
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Utilisateur</th>
+              <th>Date</th>
+              <th>Total</th>
+              <th>Livrée</th>
+              <th>Méthode de paiement</th>
+              <th>Détails de la commande</th>
+            </tr>
+          </thead>
+          <tbody>
+            {['bi bi-check-lg text-success', 'bi bi-x-lg text-danger'].map((item, idx) => (
+               <tr key={idx}>
+              <td key={idx+1}>1</td>
+              <td>Mark Twain</td>
+              <td>04/01/2023</td>
+                <td>254 €</td>
+             
+              <td>
+                <i className={item}></i>
+                </td>
+                <td>Paypal</td>
+              <td>
+                <Link to='/admin/order-details'>Accéder aux détails</Link>
+              </td>
+            </tr>
+            ))}
+           
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  )
 }
 
 export default AdminOrdersPage
