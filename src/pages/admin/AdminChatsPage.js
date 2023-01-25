fe8a971ca30aaa55523cbcc568e0f6b3fbeb3d40
dhagnere <da.hagnere@gmail.com>
2023-01-25 10:23:42 +0100

    Create ChatPage for admin
    + AdminChatRoomComponent

--- src/pages/admin/AdminChatsPage.js
+++ src/pages/admin/AdminChatsPage.js
@@ -1,5 +1,20 @@
+import AdminChatRoomPageComponent from '../../components/admin/AdminChatRoomComponent'
+import AdminLinksComponent from '../../components/admin/AdminLinksComponent'
+import { Row, Col } from 'react-bootstrap'
+
 const AdminChatsPage = () => {
-  return <p>This is Admin Chats Page</p>
+  return (
+
+      <Row className="m-5">
+        <Col md={2}>
+          <AdminLinksComponent />
+        </Col>
+        <Col md={10}>
+          <AdminChatRoomPageComponent />
+        </Col>
+      </Row>
+
+  )
 }
 
 export default AdminChatsPage
