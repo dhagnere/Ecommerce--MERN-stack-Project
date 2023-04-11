d38e81e4b1bd6d6708ef133f38751e25a4d408ed
dhagnere <da.hagnere@gmail.com>
2023-04-11 10:26:38 +0200

    creating reviews + user seeder
    installing one to many relation for reviews (one user can have several review)
    installing NPM bcrypt to hash password in user model before sending to dB

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -3,16 +3,41 @@ connectDB()
 
 const categoryData = require('./categories')
 const productsData = require('./products')
+const reviewsData = require('./reviews')
+const usersData = require('./users')
 
 const Category = require('../models/CategoryModel')
 const Product = require('../models/ProductModel')
+const Review = require('../models/ReviewModel')
+const User = require('../models/UserModel')
 
 const importData = async () => {
   try {
+    await Category.collection.dropIndexes()
+    await Product.collection.dropIndexes()
+    await Review.collection.dropIndexes()
+    await User.collection.dropIndexes()
+
     await Category.deleteMany({})
-    await Category.insertMany(categoryData)
     await Product.deleteMany({})
-    await Product.insertMany(productsData)
+    await Review.deleteMany({})
+    await User.deleteMany({})
+
+    await Category.insertMany(categoryData)
+    await User.insertMany(usersData)
+
+    const reviews = await Review.insertMany(reviewsData)
+
+    //on parcours l'array de produits et on ajoute les reviews correspondantes
+    const sampleProducts = productsData.map((product) => {
+      reviews.map((review) => {
+        product.reviews.push(review._id)
+      })
+      return { ...product }
+    })
+
+    await Product.insertMany(sampleProducts)
+
     console.log('Seeder data imported!')
     process.exit()
   } catch (error) {
